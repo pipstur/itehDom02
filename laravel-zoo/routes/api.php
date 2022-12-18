08abbc648f71223fc1dfec35d4d854fafc24f384
vojislav <vojislavstevanovic171@gmail.com>
2022-12-18 13:37:05 +0100

    loginovanje valjda radi sad, al logautovanje ne radi, svakako nemam vise vremena danas

--- laravel-zoo/routes/api.php
+++ laravel-zoo/routes/api.php
@@ -23,6 +23,7 @@ Route::resource('zoo', ZooController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
