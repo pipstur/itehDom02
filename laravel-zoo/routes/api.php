27b87030e14abc7c1260f6db774dd3eb08ac393e
vojislav <vojislavstevanovic171@gmail.com>
2022-12-19 13:13:22 +0100

    grupisane komande u auth:sanctum

--- laravel-zoo/routes/api.php
+++ laravel-zoo/routes/api.php
@@ -23,10 +23,16 @@ Route::resource('zoo', ZooController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+# Route::post('/logout', [AuthController::class, 'logout']);
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('drzava', DrzavaController::class);
+    Route::resource('zoo', ZooController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
