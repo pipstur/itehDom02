0704d487837376ea7b128ff182001ed61727deb0
vojislav <vojislavstevanovic171@gmail.com>
2022-12-19 20:15:31 +0100

    nešto je petljano, pišem dokumentaciju sad pa sam ponešto i ispravio samo kako bi radilo kasnije

--- laravel-zoo/app/Http/Controllers/ZooController.php
+++ laravel-zoo/app/Http/Controllers/ZooController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Resources\ResursZoo;
 use App\Models\Zoo;
+use Illuminate\Support\Facades\Validator;
 
 class ZooController extends Controller
 {
@@ -16,5 +17,32 @@ class ZooController extends Controller
     public function show(Zoo $zoo)
     {
         return new ResursZoo($zoo);
+    }
+    
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'naziv' => 'required|string',
+            'drzava_id' => 'required|integer',
+            'adresa' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+       $zoo = Zoo::create([
+            'naziv' => $request->naziv,
+            'drzava_id' => $request->drzava_id,
+            'adresa' => $request->adresa
+       ]);
+
+
+        return response()->json(['Novi Zoo kreiran', new ResursZoo($zoo)]);
+    }
+    public function destroy(Zoo $zoo)
+    {
+        $zoo->delete();
+        return response()->json(['Država uspešno obrisana.', new ResursZoo($zoo)]);
     }
 }
