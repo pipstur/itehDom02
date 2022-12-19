6e11932fc3c2855a300e98ff6eccfaf0a6c36ab7
vojislav <vojislavstevanovic171@gmail.com>
2022-12-19 19:02:04 +0100

    u drzavi dodat destroy, ali apdjet ne radi i dalje

--- laravel-zoo/app/Http/Controllers/DrzavaController.php
+++ laravel-zoo/app/Http/Controllers/DrzavaController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Drzava;
 use App\Http\Resources\ResursDrzava;
+use Illuminate\Support\Facades\Validator;
+
 class DrzavaController extends Controller
 {
     public function index(){
@@ -18,5 +20,51 @@ class DrzavaController extends Controller
         }
         return response()->json($drzava);
     }
-    
+    public function update(Request $request, Drzava $drzava)
+    {
+        $validator = Validator::make($request->all(), [
+            'ime' => 'required|string',
+            'broj_stanovnika' => 'required|integer',
+            'predsednik' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $drzava->ime = $request->ime;
+        $drzava->broj_stanovnika = $request->broj_stanovnika;
+        $drzava->predsednik = $request->predsednik;
+
+        $drzava->save();
+
+        return response()->json(['Država uspešno izmenjena.', new ResursDrzava($drzava)]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'ime' => 'required|string',
+            'broj_stanovnika' => 'required|integer',
+            'predsednik' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+       $drzava = Drzava::create([
+            'ime' => $request->ime,
+            'broj_stanovnika' => $request->broj_stanovnika,
+            'predsednik' => $request->predsednik
+       ]);
+
+
+        return response()->json(['Drzava kreirana', new ResursDrzava($drzava)]);
+    }
+    public function destroy(Drzava $drzava)
+    {
+        $drzava->delete();
+        return response()->json(['Država uspešno obrisana.', new ResursDrzava($drzava)]);
+    }
 }
